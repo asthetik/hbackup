316e47131cc950f4dbc80a79660ef2b638b8c27c
asthetik <asthetik.sun@outlook.com>
2025-06-20 16:01:56 +0800

    fix: Update build script to include additional targets and improve compression handling
    (cherry picked from commit 1b33a74da2e8866863fae95b951b810f040cfcf1)

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -8,7 +8,9 @@ echo "version: ${VERSION}"
 
 targets=(
     "x86_64-unknown-linux-gnu"
+    "x86_64-unknown-linux-musl"
     "aarch64-unknown-linux-gnu"
+    "aarch64-unknown-linux-musl"
     "x86_64-pc-windows-gnu"
     "x86_64-apple-darwin"
     "aarch64-apple-darwin"
@@ -26,15 +28,9 @@ for file in dist/bk-v*.tar.gz; do
   fi
 done
 
-for file in dist/bk-v*.zip; do
-  if [ -e "$file" ]; then
-    rm "$file"
-  fi
-done
-
 for target in "${targets[@]}"; do
     echo "==> Building for $target"
-    cross build --release --target "$target"
+    cross build --profile release-lto --target "$target"
 
     # Prepare output directory
     out_dir="dist/$target"
@@ -49,19 +45,8 @@ for target in "${targets[@]}"; do
     # Copy binary
     cp "target/$target/release/$bin_name" "$out_dir/"
 
-    # Set archive name
-    archive_target="$target"
-    if [[ "$target" == "x86_64-pc-windows-gnu" ]]; then
-        archive_target="windows"
-    fi
-
-    # If windows, also compress as zip
-    if [[ "$target" == *windows* ]]; then
-        (cd "$out_dir" && zip "../bk-${VERSION}-${archive_target}.zip" "$bin_name")
-    else
-      # Compress binary with version and target in filename
-      tar czf "dist/bk-${VERSION}-${archive_target}.tar.gz" -C "$out_dir" "$bin_name"
-    fi
+    # Compress binary with version and target in filename
+    tar czf "dist/bk-${VERSION}-${target}.tar.gz" -C "$out_dir" "$bin_name"
 done
 
 for target in "${targets[@]}"; do
