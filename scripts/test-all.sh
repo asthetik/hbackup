a099014ad772981acc05559bdb276a104f9800c3
asthetik <asthetik.sun@outlook.com>
2025-07-03 00:36:20 +0800

    feat: Add test-all script to automate testing across multiple targets
    (cherry picked from commit be7d565da2362e8294b872a7c672dbe4c3d371b5)

---
+++ scripts/test-all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+VERSION=$(grep '^version' Cargo.toml | head -1 | sed -E 's/version *= *"([^"]+)"/\1/')
+VERSION="v${VERSION}"
+
+echo "version: ${VERSION}"
+
+cargo fmt --all --check && cargo clippy -- -D warnings
+
+targets=(
+    "x86_64-unknown-linux-gnu"
+    "x86_64-unknown-linux-musl"
+    "aarch64-unknown-linux-gnu"
+    "aarch64-unknown-linux-musl"
+    "x86_64-pc-windows-gnu"
+    "x86_64-apple-darwin"
+    "aarch64-apple-darwin"
+)
+
+profile="release-lto"
+for target in "${targets[@]}"; do
+    echo "==> Testing for $target"
+    cross test --target "$target"
+done
+
