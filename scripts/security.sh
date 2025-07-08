e682383359ccbceddfebb629f1c9f78d2d8c3d74
asthetik <asthetik.sun@outlook.com>
2025-07-08 18:16:47 +0800

    chore: Add Windows GNU target to build and test scripts
    Adds Windows GNU target to build and test workflows
    
    Introduces support for the `i686-pc-windows-gnu` target in build and test scripts, enabling compatibility with Windows GNU environments. Updates workflows, build scripts, and test scripts accordingly to ensure proper handling of this new target. Simplifies build profiles for consistency across scripts.
    
    (cherry picked from commit 2f64e19da8c51512686581f00d47001a99bdbba1)

--- scripts/security.sh
+++ scripts/security.sh
@@ -4,15 +4,9 @@ set -e
 cargo deny --all-features check
 cargo audit --deny warnings
 
-profiles=(
-    "release"
-    "release-lto"
-)
-
-for profile in "${profiles[@]}"; do
-    echo "profile = $profile"
-    cargo auditable build --profile $profile
-    cargo audit bin ./target/$profile/bk
-done
+profile="release"
+echo "profile is: $profile"
+cargo auditable build --profile $profile
+cargo audit bin ./target/$profile/bk
 
 
