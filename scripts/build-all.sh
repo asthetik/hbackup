8ab2efd4ddc105a4a9b421bd7a785b0e19f1aa8b
asthetik <asthetik.sun@outlook.com>
2025-07-08 17:20:41 +0800

    chore: Update CI workflows and scripts for improved release process
    Refactors CI workflows and scripts for streamlined releases
    
    - Removes redundant `workflow_call` trigger and `lto` configuration.
    - Consolidates publish logic into a new `release.yml` workflow with multi-platform support, incorporating enhanced binary building and archiving.
    - Deprecates `publish.yml` workflow and updates `build-all.sh` script to align with new targets.
    - Improves release process by ensuring version consistency between tags and `Cargo.toml`.

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -9,11 +9,13 @@ echo "version: ${VERSION}"
 cargo fmt --all --check && cargo clippy -- -D warnings
 
 targets=(
-    "x86_64-unknown-linux-gnu"
+    "i686-unknown-linux-gnu"
     "x86_64-unknown-linux-musl"
-    "aarch64-unknown-linux-gnu"
-    "aarch64-unknown-linux-musl"
+    "aarch64-unknown-linux-gnu",
+    "powerpc64-unknown-linux-gnu"
+    "i686-pc-windows-msvc"
     "x86_64-pc-windows-gnu"
+    "x86_64-pc-windows-msvc"
     "x86_64-apple-darwin"
     "aarch64-apple-darwin"
 )
